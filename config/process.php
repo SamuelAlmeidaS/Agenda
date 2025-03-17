e10dcb4606b1026ea509363a2a458ff89e1f2045
Samuel Almeida <samuelmk33@gmail.com>
2025-03-17 05:42:47 -0300

    add: array

--- config/process.php
+++ config/process.php
@@ -5,9 +5,11 @@
     include_once("connection.php");
     include_once("url.php");
 
+    $contacts = [];
+
     $query = "SELECT * FROM contacts";
     $stmt = $conn->prepare($query);
     $stmt->execute();
 
-    $contscts = $stmt->fetchAll();
+    $contacts = $stmt->fetchAll();
 ?>
